1f4d8d001ae9006f9602ff196764ad60cc28c909
Development <development@bours-mbp.home>
2017-01-05 07:21:14 -0500

    tweks

--- assets/scripts/local/instagram-loader.js
+++ assets/scripts/local/instagram-loader.js
@@ -7,13 +7,9 @@
 		var igConfig = {
 			token: '199450205.2b60c7a.6b0b48cc37214274b29cf851cfdb3308',
 			userid: 'self',
-			count: 4
+			count: 10
 		};
 
-		var token = igConfig.token, // learn how to obtain it below
-	        userid = igConfig.self, // User ID - get it in source HTML of your Instagram profile or look at the next example :)
-	        num_photos = igConfig.count; // how much photos do you want to get
-	         
 	    $.ajax({
 	      url: 'https://api.instagram.com/v1/users/' + igConfig.userid + '/media/recent', // or /users/self/media/recent for Sandbox
 	      dataType: 'jsonp',
